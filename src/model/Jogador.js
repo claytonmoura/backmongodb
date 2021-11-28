6513710ca38d348822b22ab821122b6cd35ea0e8
Clayton Moura <clee.gomes@gmail.com>
2021-11-28 20:40:39 -0300

    update

--- src/model/Jogador.js
+++ src/model/Jogador.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose')
 
-const DataSchema = new mongoose.Schema({
-    nome: String,
-    email: String,
-    jogo: String,
-    telefone: {
-        type: String,
-        unique: true
-     },
-    telSecundario: {
-        type: String,
-        unique: true
-    }
-})
+const DataSchema = mongoose.Schema({
+    nome: {type: String},
+    email: {type: String},
+    jogo: {type: String},
+    telefone: {type: String,unique: true},
+    telSecundario: {type: String,unique: true}
+},{timestamps:true})
 
-const jogador = mongoose.model('Jogador', DataSchema) // falando pro mongoose criar um model usuario atraves dos campos do DataSchema
-module.exports = jogador; 
+module.exports = mongoose.model('jogador', DataSchema)
